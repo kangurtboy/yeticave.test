653d46a8bd72f07e71cf75ac093bd65a378d8ace
o_ebrahim <kangurt.boy@gmail.com>
2021-03-12 10:38:15 +0500

    Перебирал массив с обявлениями на странице с помощю цикла foreach

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $user_avatar = 'img/user.jpg';
 
 $categories = ['Доски и лыжи' , "Крепления" , "Ботинки" , "Одежда" , "Инструменты" , "Разное"];
 
-$ads = [
+$lots = [
 	['name' => "2014 Rossignol District Snowboard",
 		'category' => "Доски и лыжи",
 		'price' => 10999,
@@ -119,24 +119,27 @@ $ads = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+			<!-- Перебор обявлении -->
+			<?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src=<?=$lot['img_url']?> width="350" height="260" alt=<?=$lot['category']?>>
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$lot['category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['name']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$lot['price']?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
                         </div>
                     </div>
                 </div>
-            </li>
+			</li>
+			<? endforeach ?> 
         </ul>
     </section>
 </main>

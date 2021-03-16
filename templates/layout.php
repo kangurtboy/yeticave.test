1aa397a2cae90a9c433fbeb1ccbf48946524d312
o_ebrahim <kangurt.boy@gmail.com>
2021-03-16 21:40:09 +0500

    Шапка сайта тепер в шаблоне

--- templates/layout.php
+++ templates/layout.php
@@ -12,42 +12,6 @@ require_once 'index.php';
 </head>
 <body>
 
-<header class="main-header">
-    <div class="main-header__container container">
-        <h1 class="visually-hidden">YetiCave</h1>
-        <a class="main-header__logo">
-            <img src="img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
-        </a>
-        <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru">
-            <input type="search" name="search" placeholder="Поиск лота">
-            <input class="main-header__search-btn" type="submit" name="find" value="Найти">
-        </form>
-        <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
-
-        <nav class="user-menu">
-			<!-- здесь должен быть PHP код для показа аватара пользователя -->
-			<?php if($is_auth) : ?>
-			<div class="user-menu__image">
-				<img src="<?=$user_avatar?>" alt="Ползователь">
-			</div>
-			<div class="user-menu__logged">
-				<p><?=$user_name?></p>
-			</div>
-			<?else : ?>
-
-				<ul class="user-menu__list">
-					<li class="user-menu__item">
-						<a href="#">Регистрация</a>
-					</li>
-					<li class="user-menu__item">
-						<a href="#">Вход</a>
-					</li>
-				</ul>
-			<?php endif ?>
-        </nav>
-    </div>
-</header>
-
 <main class="container">
 <?=$main_content ?>
 </main>

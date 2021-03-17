265572779a97041480a1ef7ecc912fa1dc304dd5
o_ebrahim <kangurt.boy@gmail.com>
2021-03-17 21:16:02 +0500

    Cоздал функция для подсчета таймера лотов пока он считает до полуночи

--- functions.php
+++ functions.php
@@ -23,3 +23,17 @@ function price_format($num)
 		return $num;
 	}
 };
+
+function timer(){
+ //Таймер для потсчета время лотый
+ $current_time = getdate();
+
+ $hours_in_day = 24;
+ $minutes_in_hour = 60;
+
+ $hour_left =  $hours_in_day -  $current_time['hours'];
+ $minutes_left = $minutes_in_hour - $current_time['minutes'];
+
+ return $hour_left . ' : ' . $minutes_left;
+
+};

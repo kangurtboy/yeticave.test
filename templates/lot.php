a97598270e7bca0f22a3b8c288086f3267965426
o_ebrahim <kangurt.boy@gmail.com>
2021-03-17 17:22:54 +0500

    Немножка отфилтровал данные лоты

--- templates/lot.php
+++ templates/lot.php
@@ -8,11 +8,11 @@ $lot = $arr;
 	 </div>
 	 <div class="lot__info">
 		 <span class="lot__category"><?=$lot['category']?></span>
-		 <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['name']?></a></h3>
+		 <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($lot['name'])?></a></h3>
 		 <div class="lot__state">
 			 <div class="lot__rate">
 				 <span class="lot__amount">Стартовая цена</span>
-				 <span class="lot__cost"><?=price_format($lot['price'])?></span>
+				 <span class="lot__cost"><?=htmlspecialchars( price_format($lot['price']))?></span>
 			 </div>
 			 <div class="lot__timer timer">
 

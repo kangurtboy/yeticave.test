6c2501ae190e426105346e385cdb7abedd1e4ed0
o_ebrahim <kangurt.boy@gmail.com>
2021-04-24 20:32:50 +0500

    Добавил адрес корневые директории

--- config.php
+++ config.php
@@ -1,3 +1,11 @@
 <?php 
+
+//Часовой пояс
 	date_default_timezone_set('Asia/Dushanbe');
+
+//Корневой адрес сервера понадобится для подключение ресурсов
+	$sever_dir = 'http://' . $_SERVER['SERVER_NAME'] . '/';
+//Корневой путь сервера для подключение php сценарии
+	$documet_root = $_SERVER['DOCUMENT_ROOT'];
+
 ?>

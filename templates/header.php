70435ee4688e331e1bc36ffaa9a3767cf75b5035
o_ebrahim <kangurt.boy@gmail.com>
2021-04-26 17:13:19 +0500

    Исправил ссылки

--- templates/header.php
+++ templates/header.php
@@ -15,7 +15,7 @@ $user = $arr;
 <body>
 
 <header class="main-header">
-    <div class="main-header__container container">
+    <div class="main-header__container container">`
         <h1 class="visually-hidden">YetiCave</h1>
         <a class="main-header__logo">
             <img src=<?=$server_name .'img/logo.svg'?> width="160" height="39" alt="Логотип компании YetiCave">
@@ -24,7 +24,7 @@ $user = $arr;
             <input type="search" name="search" placeholder="Поиск лота">
             <input class="main-header__search-btn" type="submit" name="find" value="Найти">
         </form>
-        <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
+        <a class="main-header__add-lot button" href=<?=$server_name . '/pages/add.php'?>>Добавить лот</a>
 
         <nav class="user-menu">
 			<!-- здесь должен быть PHP код для показа аватара пользователя -->

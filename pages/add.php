da043214d13884b845b1c733b895a5907a0d0891
o_ebrahim <kangurt.boy@gmail.com>
2021-04-29 18:50:26 +0500

    Сделал базовые валидации поля

--- pages/add.php
+++ pages/add.php
@@ -2,16 +2,70 @@
 require_once '../functions.php';
 require_once '../data.php';
 require_once '../config.php';
-?>
+//Валидация формы
+$errors = [];
+$dict = [
+	'lot-name'=> 'Наименование',
+	'category'=> 'Категория',
+	'message'=> 'Описание',
+	'img'=> 'Изображение',
+	'lot-rate'=> 'Начальная цена',
+	'lot-step'=> 'Шаг ставки',
+	'lot-date'=> 'Дата окончания торгов'
+];
 
-<!-- Отрисовка шапки -->
-<?=template_render('/templates/header.php' , $user)?>
-<?=template_render('/templates/nav.php' , [])?>
+$int_filelds = ['lot-rate' , 'lot-step'];
 
-<!-- Отрисовка формы -->
+if(!empty($_POST)){
 
-<?=template_render('/templates/add_form.php' , [])?>
+	$lot = $_POST;
 
-<!-- Отрисовка футер -->
-<?=template_render('/templates/footer.php', $categories);
+	foreach($lot as $key=>$fild){
+		//Проверка на пустота
+		if(empty($fild)){
+			$errors[$key] = 'Заполните это поля пожалуйста';
+		}
+	}
+
+	if (!in_array($lot['category'] , $categories)){
+		//Проверка Категории
+		$errors['category']  = "Выберите категорию";
+	}
+
+	foreach ($int_filelds as $fild){
+		//Проверка чисел
+
+		if(!filter_var($lot[$fild] , FILTER_VALIDATE_INT)){
+			$errors[$fild] = "Введите только число";
+		}
+	}
+	//Проверка Дата
+	$date = date_parse($lot['lot-date']);
+
+	if(!$date['year'] && !$date['month'] && !$date['day']){
+		$errors['lot-date'] = 'Введите дату пожалуйста';
+	}
+
+	
+
+};
+
+
+
+//Отрисовка шапки
+ echo template_render('/templates/header.php' , $user);
+ echo template_render('/templates/nav.php' , []);
+
+
+	//Отрисовка карточта лота
+	//echo template_render('/templates/lot.php' , []);
+
+
+	//Отрисовка формы
+	echo template_render('/templates/add_form.php' , $errors);
+
+
+//Отрисовка футер
+echo template_render('/templates/footer.php', $categories);
+
 ?>
--- templates/add_form.php
+++ templates/add_form.php
@@ -1,17 +1,22 @@
+<!-- Щаблон формы -->
+<?php 
+$errors = $arr;
+?>
+
 <main>
 
-  <form class="form form--add-lot container form--invalid" action="https://echo.htmlacademy.ru" method="post"> <!-- form--invalid -->
+  <form class="form form--add-lot container <?= empty($errors) ? '' : 'form--invalid'?>" action="add.php" method="post" name="lot"> <!-- form--invalid -->
     <h2>Добавление лота</h2>
     <div class="form__container-two">
-      <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+      <div class="form__item  <?=$_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST['lot-name'])? 'form__item--invalid': '' ?>"> <!-- form__item--invalid -->
         <label for="lot-name">Наименование</label>
-        <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
-        <span class="form__error">Введите наименование лота</span>
+        <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" value=<?=$_POST['lot-name']?>>
+        <span class="form__error"><?=$errors['lot-name']?? "Введите наименование лота"?></span>
       </div>
-      <div class="form__item">
+      <div class="form__item <?=$_SERVER['REQUEST_METHOD'] == 'POST' && empty(!$errors['category'])? 'form__item--invalid': '' ?>">
         <label for="category">Категория</label>
-        <select id="category" name="category" required>
-          <option>Выберите категорию</option>
+        <select id="category" name="category" >
+          <option selected><?=$_POST['category']??'Выберите категорию'?></option>
           <option>Доски и лыжи</option>
           <option>Крепления</option>
           <option>Ботинки</option>
@@ -22,41 +27,41 @@
         <span class="form__error">Выберите категорию</span>
       </div>
     </div>
-    <div class="form__item form__item--wide">
+    <div class="form__item form__item--wide <?=$_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST['message'])? 'form__item--invalid': '' ?>">
       <label for="message">Описание</label>
-      <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
+      <textarea id="message" name="message" placeholder="Напишите описание лота" ><?=$_POST['message']?></textarea>
       <span class="form__error">Напишите описание лота</span>
     </div>
-    <div class="form__item form__item--file"> <!-- form__item--uploaded -->
+    <div class="form__item form__item--file <?=$_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST['img'])? 'form__item--invalid': '' ?>"> <!-- form__item--uploaded -->
       <label>Изображение</label>
       <div class="preview">
         <button class="preview__remove" type="button">x</button>
         <div class="preview__img">
-          <img src="img/avatar.jpg" width="113" height="113" alt="Изображение лота">
+          <img src="<?=$_POST['img']?>" width="113" height="113" alt="Изображение лота">
         </div>
       </div>
       <div class="form__input-file">
-        <input class="visually-hidden" type="file" id="photo2" value="">
+        <input class="visually-hidden" type="file" id="photo2" value="" name="img">
         <label for="photo2">
           <span>+ Добавить</span>
         </label>
       </div>
     </div>
     <div class="form__container-three">
-      <div class="form__item form__item--small">
+      <div class="form__item form__item--small <?=$_SERVER['REQUEST_METHOD'] == 'POST' && empty(!$errors['lot-rate'])? 'form__item--invalid': '' ?>">
         <label for="lot-rate">Начальная цена</label>
-        <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
-        <span class="form__error">Введите начальную цену</span>
+        <input id="lot-rate" type="number" name="lot-rate" placeholder="0" value=<?=$_POST['lot-rate']?>>
+        <span class="form__error"> <?=$errors['lot-rate'] ?? 'Введите начальную цену'?></span>
       </div>
-      <div class="form__item form__item--small">
+      <div class="form__item form__item--small <?=$_SERVER['REQUEST_METHOD'] == 'POST' && empty(!$errors['lot-step'])? 'form__item--invalid': '' ?>">
         <label for="lot-step">Шаг ставки</label>
-        <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
-        <span class="form__error">Введите шаг ставки</span>
+        <input id="lot-step" type="number" name="lot-step" placeholder="0" value=<?=$_POST['lot-step']?> >
+        <span class="form__error"> <?=$errors['lot-step'] ??'Введите шаг ставки' ?></span>
       </div>
-      <div class="form__item">
+      <div class="form__item <?=$_SERVER['REQUEST_METHOD'] == 'POST' && empty(!$errors['lot-date'])? 'form__item--invalid': '' ?>">
         <label for="lot-date">Дата окончания торгов</label>
-        <input class="form__input-date" id="lot-date" type="date" name="lot-date" required>
-        <span class="form__error">Введите дату завершения торгов</span>
+        <input class="form__input-date" id="lot-date" type="date" name="lot-date" value=<?=$_POST['lot-date']?>>
+        <span class="form__error"><?=$errors['lot-date']?></span>
       </div>
     </div>
     <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>

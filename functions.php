353a8c97c895ed3e9f53c68b82857471ca2c1979
o_ebrahim <kangurt.boy@gmail.com>
2021-06-02 10:51:46 +0500

    Фунции для работы с mysql

--- functions.php
+++ functions.php
@@ -102,3 +102,62 @@ function upload_img($img , $upload_path){
 	}
 	return $error;
 }
+
+/**
+ * Создает подготовленное выражение на основе готового SQL запроса и переданных данных
+ *
+ * @param $link mysqli Ресурс соединения
+ * @param $sql string SQL запрос с плейсхолдерами вместо значений
+ * @param array $data Данные для вставки на место плейсхолдеров
+ *
+ * @return mysqli_stmt Подготовленное выражение
+ */
+function db_get_prepare_stmt($link, $sql, $data = []) {
+    $stmt = mysqli_prepare($link, $sql);
+
+    if ($data) {
+        $types = '';
+        $stmt_data = [];
+
+        foreach ($data as $value) {
+            $type = null;
+
+            if (is_int($value)) {
+                $type = 'i';
+            }
+            else if (is_string($value)) {
+                $type = 's';
+            }
+            else if (is_double($value)) {
+                $type = 'd';
+            }
+
+            if ($type) {
+                $types .= $type;
+                $stmt_data[] = $value;
+            }
+        }
+
+        $values = array_merge([$stmt, $types], $stmt_data);
+
+        $func = 'mysqli_stmt_bind_param';
+        $func(...$values);
+    }
+
+    return $stmt;
+}
+
+function mysql_simple ($sql , $arr=[]){
+	//Упрашеная функция для работы с MySQL
+	require 'config.php';
+
+	$stmt = db_get_prepare_stmt($connection, $sql, $arr);
+	mysqli_stmt_execute($stmt);
+
+	$result = mysqli_stmt_get_result($stmt);
+
+	if ($result) {
+		$received_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	}
+	return $received_data;
+}

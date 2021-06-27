bd2c8ed71f7316a36904dc954d2388079768df40
o_ebrahim <kangurt.boy@gmail.com>
2021-06-27 22:48:04 +0500

    Реализация пагинация в главном странице

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
+//Главная страница
 require_once 'lots.php';
 require_once 'functions.php';
 require_once 'config.php';
 require_once 'data.php';
-//Главная страница
+//Пагинация
+$cur_page = $_GET['page'] ?? 1;
+$items_count = mysql_simple('SELECT count(*) FROM lots' , [])[0]['count(*)'];
+$items_limit = 6;
+$page_count = (int) ceil($items_count / $items_limit); 
+$offset = ($cur_page - 1) * $items_limit;
 
-$lots_query = mysqli_query($connection , 'SELECT * FROM lots ORDER BY crate_date DESC');
-$open_lots = mysqli_fetch_all($lots_query, MYSQLI_ASSOC);
+
+
+$lots_query = mysqli_query($connection , "SELECT * FROM lots ORDER BY crate_date DESC LIMIT $items_limit OFFSET $offset");
+if($lots_query){
+
+	$open_lots = mysqli_fetch_all($lots_query, MYSQLI_ASSOC);
+}
 
 $page_content = template_render('./templates/index.php' , $open_lots);
 
@@ -14,7 +25,14 @@ $content = template_render('./templates/layout.php' , [
 	'title' => 'Главная',
 	'main_content' => $page_content,
 	'nav'=> [],
-	'pagination' => []
+	'pagination' => $page_count
 ]);
+
+if($cur_page < 1 || $cur_page > $page_count){
+	$content = template_render('./templates/layout.php' , [
+		'title' => 'Страница не найдена',
+		'main_content' => template_render('./templates/error.php', ['message'=> 'Страница не найдена']),
+	]);
+}
 
 print($content);
--- templates/pagination.php
+++ templates/pagination.php
@@ -1,14 +1,28 @@
 <?php 
 //Пагинация
+$cur_page = $_GET['page'] ?? 1;
+$next = $cur_page;
+$prev = $cur_page;
+
+if($cur_page > 1){
+
+	$prev = $cur_page - 1;
+}
+if($cur_page < $arr){
+
+	$next = $cur_page + 1;
+}
+
 ?>	
 
-<?php if(isset($arr) && count($arr)>1) : ?>
+<?php if(isset($arr) && $arr>1) : ?>
 	<ul class="pagination-list">
-      <li class="pagination-item pagination-item-prev"><a>Назад</a></li>
-	  <?php foreach ($arr as  $id=>$item): ?>
-      <li class="pagination-item "><a>1</a></li> <!-- pagination-item-active -->
-      <li class="pagination-item"><a href="#"><?=$id?></a></li>
-	  <?php endforeach ?>
-      <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
+      <li class="pagination-item pagination-item-prev"><a href=<?="/?page=$prev"?>>Назад</a></li>
+
+	  <?php for ($i = 1 ; $i< $arr+1; $i++): ?>
+      <li class="pagination-item <?=$cur_page == $i ? 'pagination-item-active': '' ?>"><a href=<?='/?page='. $i?>><?=$i?></a></li> <!-- pagination-item-active -->
+
+	  <?php endfor ?>
+      <li class="pagination-item pagination-item-next"><a href=<?="/?page=$next"?>>Вперед</a></li>
     </ul>
 <?php endif?>
